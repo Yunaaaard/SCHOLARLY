8b38e5d9aea82ab930538cf49c0e6cbae97f6920
Yunaaaard <tarimanleonard28@gmail.com>
2025-12-16 20:16:50 +0800

    small fix

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -60,7 +60,6 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ] : [],
         ],
 
